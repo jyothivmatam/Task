8c40a0890d5aabcc3deab7e299af842087f5831e
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-28 00:34:13 +0530

    Add files via upload

---
+++ Program2/Program2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            char ch = Convert.ToChar(Console.ReadLine());
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ch)
+                {
+                    Console.WriteLine("yes");
+                }
+            }          
+          }
+            
+    }
+}
+
+
+
+
+
+
+
+
+
