6f500cd299b91f1cd92a21a96675502fdf95b25c
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-28 10:33:07 +0530

    Add files via upload

---
+++ CharacterWord/CharacterWord/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char c1=Convert.ToChar(Console.ReadLine());
+            string str2=Char.ToString(c1);
+            if(str2.Contains(str));
+            Console.WriteLine("true");
+            
+            Console.ReadLine();
+        }
+    }
+}
